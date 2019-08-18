0a82e33fe6563e9d13874a137e65c4cde1fb1c00
RenatoWw <renatoflud2017@hotmail.com>
2019-08-18 11:26:45 -0300

    edit button, refactored code and 'what i've done'

--- js/main.js
+++ js/main.js
@@ -1,29 +1,48 @@
 const ul = document.querySelector('#tasks');
 const addTask = document.querySelector('#addTask');
-let taskInput = document.querySelector('#newTask');
+const taskInput = document.querySelector('#newTask');
+const completedTask = document.querySelector('#completed-tasks');
 
-addTask.addEventListener('click', () => {
-    if(taskInput.value == ''){
-        console.log('beep boop');
-    } else {
-        const items = ul.appendChild(document.createElement('li'));
-        const completeButton = document.createElement('button');
-        items.innerHTML = taskInput.value;
-        items.appendChild(completeButton).innerHTML = 'Finish';
-        items.appendChild(completeButton).style.cssFloat = 'right';
-        items.appendChild(completeButton).style.marginTop = '3.5px';
-        items.appendChild(completeButton).style.marginRight = '5px';
-        items.appendChild(completeButton).style.borderRadius = '4px';
-        completeButton.addEventListener('click', () => {
-            let item = completeButton.parentNode;
-            ul.removeChild(item);
-        });
-    }
+addTask.onclick = () => {
+    taskInput.value == '' ? console.log('beep boop') : createNewTask();
     taskInput.value = '';
     taskInput.focus();
-});
+};
 
+const createNewTask = () => {
+    // creating task li
+    const items = ul.appendChild(document.createElement('li'));
+    items.innerHTML = taskInput.value;
+    // creating delete button
+    const completeButton = document.createElement('button');
+    items.appendChild(completeButton).innerHTML = 'Finish';
+    items.appendChild(completeButton).style.cssFloat = 'right';
+    completeButton.onclick = () => {
+        deleteTask(completeButton)
+        items.appendChild(completeButton).style.display = 'none';
+        items.appendChild(editButton).style.display = 'none';
+        completedTask.appendChild(items);
+    };
+    // creating edit button
+    const editButton = document.createElement('button');
+    items.appendChild(editButton).innerHTML = 'Edit';
+    items.appendChild(editButton).style.cssFloat = 'right';
+    editButton.onclick = () => editTask(items, completeButton, editButton);
+}
 
-// TODO: seção 'what i've done'
-// TODO: botão editar que abre um prompt para usuário escrever novo nome
+const deleteTask = (tag) => {
+    let item = tag.parentNode;
+    ul.removeChild(item);
+}
+
+const editTask = (tag, tag2, tag3) => {
+    const name = prompt('New name');
+    tag.innerHTML = name;
+    tag.appendChild(tag2)
+    tag.appendChild(tag2).innerHTML = 'Finish';
+    tag.appendChild(tag2).style.cssFloat = 'right';
+    tag.appendChild(tag3).innerHTML = 'Edit';
+    tag.appendChild(tag3).style.cssFloat = 'right';
+}
+
 // TODO: refatorar esse código feio com funções, legibilidade e organização
